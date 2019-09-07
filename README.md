f6eac2489fc973f6b0c662fc9dbf039bd20ca432
Alec Masterson <alecjmasterson@gmail.com>
2019-09-07 15:08:53 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-alecmasterson.github.io
---
+++ precache-manifest.d0c204d2f5382b962f02b921c45c07d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34867dbe89eca8c470bad6a152d21fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bf8d8d684e5e8558578",
+    "url": "/static/js/2.c0daf271.chunk.js"
+  },
+  {
+    "revision": "2e34b4a030c9a8f36f9a",
+    "url": "/static/js/main.05f630cd.chunk.js"
+  },
+  {
+    "revision": "155db51ba58beb1d5bd4",
+    "url": "/static/js/runtime~main.623f06b0.js"
+  }
+]);
