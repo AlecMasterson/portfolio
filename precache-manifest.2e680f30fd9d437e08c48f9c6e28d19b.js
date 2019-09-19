a26c9fd88ee591ece9138cdcbd0d2c137b7dd2ad
Alec Masterson <alecjmasterson@gmail.com>
2019-09-18 20:45:07 -0500

    Updates

---
+++ precache-manifest.2e680f30fd9d437e08c48f9c6e28d19b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a46139c3ebae0acd510b44abaf6779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "367526fd42d7840e7ce6",
+    "url": "/static/css/main.9ee228d6.chunk.css"
+  },
+  {
+    "revision": "1a22715529b4a7c51f35",
+    "url": "/static/js/2.a8c502b7.chunk.js"
+  },
+  {
+    "revision": "367526fd42d7840e7ce6",
+    "url": "/static/js/main.4f91dc6b.chunk.js"
+  },
+  {
+    "revision": "2cfa9142ac539e526711",
+    "url": "/static/js/runtime~main.84d7cecc.js"
+  }
+]);
